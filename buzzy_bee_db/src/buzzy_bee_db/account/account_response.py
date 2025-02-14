767377e6574d3efcb982ac2e175f7fb34a55ae06
MattColb <colbertmatt12+6ymv9vew@gmail.com>
2025-02-14 11:47:59 -0600

    Sub Accounts DB completion

--- buzzy_bee_db/src/buzzy_bee_db/account/account_response.py
+++ buzzy_bee_db/src/buzzy_bee_db/account/account_response.py
@@ -1,15 +1,23 @@
 from buzzy_bee_db.db_response import DB_Response
 
-class AccountResponse(DB_Response):
-    def __init__(self, success=None, message=None, user_id=None):
+class SubAccountResponse(DB_Response):
+    def __init__(self, sub_user_id=None, success=None, message=None):
         super().__init__(success, message)
-        self.user_id = user_id
+        self.sub_user_id = sub_user_id
 
-    def get_user_id(self):
-        return self.user_id
+    def get_sub_user_id(self):
+        return self.sub_user_id
 
-    def set_user_id(self, user_id):
-        self.user_id = user_id
+    def set_sub_user_id(self, sub_user_id):
+        self.sub_user_id = sub_user_id
 
-    def __repr__(self):
-        return f"{self.user_id}, {self.message}, {self.success}"
+class SubAccountGetResponse(DB_Response):
+    def __init__(self, users=None, success=None, message=None):
+        super().__init__(success, message)
+        self.users = users
+
+    def get_users(self):
+        return self.users
+
+    def set_users(self, users):
+        self.users = users
---
+++ buzzy_bee_db/src/buzzy_bee_db/account/sub_account.py
@@ -0,0 +1,108 @@
+from buzzy_bee_db.account.account_response import SubAccountGetResponse, SubAccountResponse
+import uuid
+import boto3
+
+ddb_client = boto3.client("dynamodb")
+
+def get_sub_accounts(main_account_id, ddb_table_name):
+    result = ddb_client.get_item(
+        TableName=ddb_table_name,
+        Key={
+            "user_id":{"S":main_account_id}
+        }
+    )
+
+    sub_users = result["Item"].get("sub_users", {})
+    users = sub_users.get("L", [])
+
+    if result['ResponseMetadata']['HTTPStatusCode'] == 200:
+        return SubAccountGetResponse(users=users, success=True, message=None)
+
+    return SubAccountGetResponse(users=None, success=False, message="Something went wrong when updating the table")
+
+def create_sub_account(main_account_id, ddb_table_name, sub_account_name, sub_account_grade):
+    sub_account_id = str(uuid.uuid4())
+    sub_account = {"L":[
+        {"M":{
+            "user_id":{"S":sub_account_id},
+            "name":{"S":sub_account_name},
+            "grade":{"S":sub_account_grade}
+        }}
+    ]}
+
+    result = ddb_client.update_item(
+        TableName = ddb_table_name,
+        Key = {
+            "user_id":{"S":main_account_id},
+        },
+        UpdateExpression="SET sub_users = list_append(sub_users, :i)",
+        ExpressionAttributeValues={
+            ":i":sub_account
+        }
+    )
+
+    if result['ResponseMetadata']['HTTPStatusCode'] == 200:
+        return SubAccountResponse(sub_user_id=sub_account_id, success=True, message=None)
+
+    return SubAccountResponse(sub_user_id=None, success=False, message="Something went wrong when updating the table")
+
+def update_sub_account(main_account_id, ddb_table_name, sub_account_id, sub_account_new_name, sub_account_new_grade):
+    result = ddb_client.get_item(
+        TableName=ddb_table_name,
+        Key={
+            "user_id":{"S":main_account_id}
+        }
+    )
+
+    sub_users = result["Item"].get("sub_users", {})
+    sub_users = sub_users.get("L", [])
+
+    for i in sub_users:
+        if i["M"]["user_id"]["S"] == sub_account_id:
+            i["M"]["name"]["S"] = i["M"]["name"]["S"] if sub_account_new_name == None else sub_account_new_name
+            i["M"]["grade"]["S"] = i["M"]["grade"]["S"] if sub_account_new_grade == None else sub_account_new_grade
+
+    result = ddb_client.update_item(
+        TableName = ddb_table_name,
+        Key = {
+            "user_id":{"S":main_account_id},
+        },
+        UpdateExpression="SET sub_users = :i",
+        ExpressionAttributeValues={
+            ":i":{"L":sub_users}
+        }
+    )
+
+    if result['ResponseMetadata']['HTTPStatusCode'] == 200:
+        return SubAccountResponse(sub_user_id=None, success=True, message=None)
+
+    return SubAccountResponse(sub_user_id=None, success=False, message="Something went wrong when updating the table")
+
+def delete_sub_account(main_account_id, ddb_table_name, sub_account_id):
+    result = ddb_client.get_item(
+        TableName=ddb_table_name,
+        Key={
+            "user_id":{"S":main_account_id}
+        }
+    )
+
+    sub_users = result["Item"].get("sub_users", {})
+    sub_users = sub_users.get("L", [])
+
+    new_sub_users = [i for i in sub_users if i["M"]["user_id"]["S"] != sub_account_id]
+
+    result = ddb_client.update_item(
+        TableName = ddb_table_name,
+        Key = {
+            "user_id":{"S":main_account_id},
+        },
+        UpdateExpression="SET sub_users = :i",
+        ExpressionAttributeValues={
+            ":i":{"L":new_sub_users}
+        }
+    )
+
+    if result['ResponseMetadata']['HTTPStatusCode'] == 200:
+        return SubAccountResponse(sub_user_id=None, success=True, message=None)
+
+    return SubAccountResponse(sub_user_id=None, success=False, message="Something went wrong when updating the table")
