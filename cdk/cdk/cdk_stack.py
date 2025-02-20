d6ac720d446e52c42326caba71274c2588c99192
MattColb <colbertmatt12+6ymv9vew@gmail.com>
2025-02-20 15:17:34 -0600

    Adding commits

--- cdk/cdk/cdk_stack.py
+++ cdk/cdk/cdk_stack.py
@@ -6,7 +6,6 @@ from aws_cdk import (
     aws_ecs_patterns,
     aws_ecs,
     CfnOutput,
-    aws_docdb as docdb,
     aws_ec2
     # aws_sqs as sqs,
 )
@@ -19,30 +18,19 @@ class CdkStack(Stack):
 
         # The code that defines your stack goes here
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
-
-
         #Create a documentdb with a mongodb connection and attach it to the fargate service
         vpc = aws_ec2.Vpc(self, "MyVpc", max_azs=2)
 
-        # Create a DocumentDB cluster
-        # db_cluster = docdb.DatabaseCluster(
-        #     self, "MyDocDBCluster",
-        #     master_user=docdb.Login(
-        #         username="docdb_admin",
-        #         secret_name="/myapp/mydocdb/masteruser"
-        #     ),
-        #     instance_type=aws_ec2.InstanceType.of(
-        #         aws_ec2.InstanceClass.BURSTABLE2, 
-        #         aws_ec2.InstanceSize.SMALL
-        #     ),
-        #     vpc=vpc,
-        #     removal_policy=RemovalPolicy.DESTROY,
-            
+        #Create EC2 with mongodb
+        # ec2_instance = aws_ec2.Instance(self, "MyInstance",
+        #     instance_name="MyMongoDB",
+        #     vpc=vpc, 
+        #     instance_type=aws_ec2.InstanceType("t2.micro"),
+        #     machine_image=aws_ec2.MachineImage.latest_amazon_linux(),
+        #     key_name="mykey",
+        #     vpc_subnets=aws_ec2.SubnetSelection(subnet_type=aws_ec2.SubnetType.PUBLIC),
+        #     security_group=aws_ec2.SecurityGroup(self, "MySecurityGroup", vpc=vpc),
+        #     user_data=aws_ec2.UserData.custom("yum install -y docker && service docker start && docker run -d -p 27017:27017 mongo")
         # )
 
         ecs_cluster = aws_ecs.Cluster(self, "MyEcsCluster", vpc=vpc)
@@ -56,17 +44,9 @@ class CdkStack(Stack):
             memory_limit_mib=512,
             task_image_options=aws_ecs_patterns.ApplicationLoadBalancedTaskImageOptions(
                 image=aws_ecs.ContainerImage.from_asset("flask_docker"),
-                # environment={
-                #     "MONGO_URI": f"mongodb://{db_cluster.cluster_endpoint.hostname}:27017"
-                # }
             ),
             public_load_balancer=True
         )
-
-        # db_cluster.connections.allow_default_port_from(load_balanced_fargate_service.service)
-
-        #Give the db cluster the ability to be accessed from my local computer
-        # db_cluster.connections.allow_default_port_from_any_ipv4()
 
         scalable_target = load_balanced_fargate_service.service.auto_scale_task_count(
             min_capacity=1,
