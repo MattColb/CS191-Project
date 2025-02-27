7e4c973e0c762669223dbb43b4afa7c65e0d9843
Lyra Leroy <lyra.leroy@drake.edu>
2025-02-27 15:54:29 -0600

    Added index, login, signup, account, sub_account, and math pages

--- flask_docker/app.py
+++ flask_docker/app.py
@@ -1,8 +1,8 @@
-import subprocess
+""" import subprocess
 
 subprocess.run("pip install --upgrade ./buzzy_bee_db".split(" "))
 from flask import Flask, request, redirect
-from pymongo import MongoClient
+subprocess.run("pip install --upgrade pymongo".split(" "))
 import os
 from flask_blueprints.login_register import login_register
 
@@ -11,4 +11,36 @@ app.secret_key="TestSecret"
 app.register_blueprint(login_register)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=80) """
+
+
+from flask import Flask, render_template, url_for, redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/account')
+def account():
+    return render_template('account.html')
+
+@app.route('/sub_account')
+def sub_account():
+    return render_template('sub_account.html')
+
+@app.route('/math')
+def math():
+    return render_template('math.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ flask_docker/static/js/script.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("BuzzyBee is ready to buzz! 🐝");
+});
